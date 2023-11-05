0aaa0c0288167dc5a56e1073038742ece8b211e0
marcosasilvalepe <61514719+marcosasilvalepe@users.noreply.github.com>
2023-11-05 15:40:06 -0300

    Update check_kilos_breakdown.js

--- scripts/check_kilos_breakdown.js
+++ scripts/check_kilos_breakdown.js
@@ -4,8 +4,8 @@ const mysql = require('mysql');
 const conn = mysql.createConnection({ 
     host: "192.168.1.90",
     port: 3306,
-    user: "dte", 
-    password: "m1Ks3DVIAS28h7dt", 
+    user: "", 
+    password: "", 
     database: "romana" 
 });
 
